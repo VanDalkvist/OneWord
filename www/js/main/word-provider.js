d6e0a8313a82365efeb30ee71602596a4373a0e7
Ivan Afanasov <i_van@list.ru>
2015-03-23 20:14:57 +0300

    moved word-provider;

---
+++ www/js/main/word-provider.js
@@ -0,0 +1,46 @@
+angular.module('one-word')
+    .factory('WordProvider', Factory);
+
+Factory.$inject = ['Word'];
+
+function Factory(Word) {
+    var words = [], current, prev, next;
+    var history = [];
+
+    return {
+        sync: _sync
+    };
+
+    function _sync() {
+        return Word.query(function (data) {
+            words = data;
+
+            return {
+                current: _current,
+                next: _next,
+                previous: _previous
+            };
+        });
+    }
+
+    function _current() {
+        return current;
+    }
+
+    function _next() {
+        history.push(angular.copy(prev));
+        prev = current;
+        var toBeCurrent = angular.copy(next);
+        Word.get(function (res) {
+            next = res;
+        });
+        return toBeCurrent;
+    }
+
+    function _previous() {
+        next = angular.copy(current);
+        current = angular.copy(prev);
+        prev = history.pop();
+        return current;
+    }
+}
