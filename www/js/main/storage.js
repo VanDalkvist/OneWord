0a7195c0ef39fd1e7573642f9da6239d45c41f29
Ivan Afanasov <i_van@list.ru>
2015-03-25 22:17:16 +0300

    added mock for word;

--- www/js/main/storage.js
+++ www/js/main/storage.js
@@ -1,7 +1,25 @@
 angular.module('one-word').service('Storage', Storage);
 
-Storage.$inject = ['Word'];
+Storage.$inject = ['$q', 'Word'];
 
-function Storage(Word) {
-    return Word;
+function Storage($q, Word) {
+
+    var WordMock = {
+        get: function () {
+            return {
+                $promise: $q.when({
+                    name: 'testWord',
+                    definition: 'This is test definition of testWord',
+                    examples: [
+                        {content: 'This is example 1'},
+                        {content: 'This is example 2'}
+                    ]
+                })
+            };
+        }
+    };
+
+    return WordMock;
+
+    //return Word;
 }
