b2681887cb4f481029b1f32e2a48498bc99c3264
Ivan Afanasov <i_van@list.ru>
2015-03-26 23:31:34 +0300

    experiments with slider;

--- www/js/word/word.controller.js
+++ www/js/word/word.controller.js
@@ -3,17 +3,41 @@
 
     angular.module('one-word').controller("WordCtrl", Controller);
 
-    Controller.$inject = ['$scope', 'word'];
+    Controller.$inject = ['$scope', '$ionicSlideBoxDelegate', 'word'];
 
-    function Controller($scope, word) {
+    function Controller($scope, $ionicSlideBoxDelegate, word) {
 
         // initialization
+        $scope.startApp = function() {
+            //$state.go('main');
+        };
 
+        var words = $scope.words = [];
+        $scope.model = {wordsCount: 0};
+        words.push({content: word, number: ++$scope.model.wordsCount});
+        words.push({content: word, number: ++$scope.model.wordsCount});
+//         $scope.slideIndex = 1;
         // model
 
-        $scope.model = word;
+        //$scope.model = word;
+
+        $scope.next = function () {
+            if ($scope.slideIndex === $scope.words.length - 2)
+                words.push({content: word, number: ++$scope.model.wordsCount});
+            $ionicSlideBoxDelegate.next();
+        };
+
+        $scope.previous = function () {
+            if ($scope.model.wordsCount >= 1)
+                $ionicSlideBoxDelegate.previous();
+        };
 
         // public functions
+
+        // Called each time the slide changes
+        $scope.slideChanged = function(index) {
+            $scope.slideIndex = index;
+        };
 
         // private functions
 
