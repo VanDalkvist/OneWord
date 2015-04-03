4e9c21386640d233a4a808bb9374ed762db2c820
Ivan Afanasov <i_van@list.ru>
2015-04-04 00:44:59 +0300

    initialization of tests;

---
+++ server/tests/words.spec.js
@@ -0,0 +1,23 @@
+// test require dependencies
+
+var should = require('should');
+var Q = require('q');
+
+// app dependencies
+
+// tests
+
+describe('words', function () {
+
+    before(function () {
+        return Q.when({});
+    });
+
+    before(function (done) {
+        done();
+    });
+
+    it('it should work', function (done) {
+        done();
+    });
+});
