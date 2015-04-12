532eb6eeacf9c639aa405ef8b30458775829b7b2
Ivan Afanasov <i_van@list.ru>
2015-04-12 21:58:58 +0300

    commented unused; clean up;

--- www/js/core/constants.js
+++ www/js/core/constants.js
@@ -1,13 +1,13 @@
 // use strict;
 
-angular.module('one-word')
-    .provider('StorageProvider', function () {
-
-        this.$get = StorageProvider;
-
-        this.$get.$inject = ['localStorageService'];
-
-        function StorageProvider(localStorageService) {
-            return localStorageService;
-        }
-    });
+//angular.module('one-word')
+//    .provider('StorageProvider', function () {
+//
+//        this.$get = StorageProvider;
+//
+//        this.$get.$inject = ['localStorageService'];
+//
+//        function StorageProvider(localStorageService) {
+//            return localStorageService;
+//        }
+//    });
--- www/js/main/main.controller.js
+++ www/js/main/main.controller.js
@@ -11,29 +11,8 @@
 
         // model
 
-        //vm.model = word;
-
         // public functions
 
-        $scope.next = _next;
-        $scope.prev = _prev;
-
         // private functions
-
-        function _next(current) {
-            /* todo
-            * 1. Save current as previous.
-            * 2. Put next as current.
-            * 3. Calculate next.
-            * */
-        }
-
-        function _prev(current) {
-            /* todo
-             * 1. Save current as next ? Or forget about current.
-             * 2. Put previous as current.
-             * 3. Calculate previous (get from history).
-             * */
-        }
     }
 })(angular.module('one-word'));
