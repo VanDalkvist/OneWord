2cdf055cf4d3b40d637cc4fca4bc6d7b32605df6
Ivan Afanasov <i_van@list.ru>
2015-04-16 21:37:55 +0300

    added swipe actions;

--- www/js/words/word.controller.js
+++ www/js/words/word.controller.js
@@ -3,9 +3,31 @@
 
     angular.module('one-word.words').controller("WordCtrl", Controller);
 
-    Controller.$inject = ['state'];
+    Controller.$inject = ['$state', 'state'];
 
-    function Controller(state) {
+    function Controller($state, state) {
+
+        // todo: if next not exist => render new empty next
+
+        var instance = this;
+
+        // view model
+
         this.vm = state;
+
+        // public functions
+
+        this.next = _next;
+        this.previous = _previous;
+
+        // private functions
+
+        function _next() {
+            $state.transitionTo('word', {name: instance.vm.next.name, direction: 'forward'});
+        }
+
+        function _previous() {
+            $state.transitionTo('word', {name: instance.vm.prev.name, direction: 'back'});
+        }
     }
 }());
