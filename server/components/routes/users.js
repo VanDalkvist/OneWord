56a4398123e099b42c3b1038ad8e5ec51c61587a
Ivan Afanasov <i_van@list.ru>
2015-04-18 13:21:01 +0300

    added CORS route middleware;

--- server/components/routes/users.js
+++ server/components/routes/users.js
@@ -1,8 +1,12 @@
 // dependencies
 
 var express = require('express');
-var router = express.Router();
 var _ = require('lodash');
+var cors = require('cors');
+var users = require("debug")('app:users');
+var util = require('util');
+
+var router = express.Router();
 
 // exports
 
@@ -12,12 +16,16 @@ module.exports.bootstrap = _bootstrap;
 
 function _bootstrap(instance) {
     var app = instance.get('app');
-    var storage = instance.get('storage');
+    var Storage = instance.get('storage');
 
-    router.post('register', function _register(req, res, next) {
+    // todo: configure cors
+    router.options('/register', cors()); // enable pre-flight request
+    router.post('/register', function _register(req, res, next) {
         var key = req.body.uid;
+        if (!key) return next(new Error("Invalid user id."));
 
-        storage.saveUser(key).then(function (res) {
+        users("We are in the 'register' route. Body is %s.", util.format(req.body));
+        Storage.saveUser(key).then(function () {
             res.status(200).send();
         }, function (err) {
             // todo: log error
@@ -28,7 +36,7 @@ function _bootstrap(instance) {
     app.use('*', function _isAuthenticated(req, res, next) {
         var authHeader = 'User-Key';
         req.userId = req.get(authHeader);
-        req.user = _getUser.call(storage, req.userId);
+        req.user = _getUser.call(Storage, req.userId);
         next();
     });
 
