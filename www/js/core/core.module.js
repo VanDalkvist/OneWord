9250801934f465896b556890afa59c2feab56ee2
Ivan Afanasov <i_van@list.ru>
2015-04-22 21:47:24 +0300

    added 'Notifications' stub;

--- www/js/core/core.module.js
+++ www/js/core/core.module.js
@@ -5,8 +5,9 @@
         .module('one-word.core', ['one-word.common', 'one-word.core.storage', 'one-word.api'])
         .config(Config);
 
-    Config.$inject = [];
+    Config.$inject = ['NotificationsProvider'];
 
-    function Config() {
+    function Config(NotificationsProvider) {
+        NotificationsProvider.setNotificationsType('local');
     }
 })();
---
+++ www/js/core/notifications/notifications.service.js
@@ -0,0 +1,23 @@
+(function () {
+
+    'use strict';
+
+    angular.module('one-word.core').service('Notifications', Service);
+
+    Service.$inject = ['Storage'];
+
+    function Service(Storage) {
+
+        // initialization
+
+        // public functions
+
+        this.method = _method;
+
+        // private functions
+
+        function _method() {
+
+        }
+    }
+})();
