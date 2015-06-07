9f9191acbe0b6c00b70e303f9e5ae1384e12752f
Ivan Afanasov <i_van@list.ru>
2015-06-07 12:53:20 +0300

    start writing scheduler for push notifications algorithm;

--- server/components/scheduler/tasks/daily-words.js
+++ server/components/scheduler/tasks/daily-words.js
@@ -1,5 +1,8 @@
 // dependencies
 
+var Q = require('q');
+var _ = require('lodash');
+
 // exports
 
 module.exports = _dailyWordsJob;
@@ -10,21 +13,47 @@ module.exports = _dailyWordsJob;
 
 function _dailyWordsJob(instance, options) {
     var db = instance.get('db');
+    var storage = instance.get('storage');
     var config = instance.get('config');
 
     var gcm = require('node-gcm');
 
-    var sender = new gcm.Sender(config['google-api']);
+    var sender = new gcm.Sender(config['google-api'].key);
 
-    // todo: generate the message
-    var message = new gcm.Message({
-        data: {}
+    _loadUsers().then(function (users) {
+        // todo: generate the message
+
+
+        var message = new gcm.Message({
+            data: {}
+        });
+
+        var messageModels = _(users).filter(function (user) {
+            return !!user.regId;
+        }).map(function (user) {
+            return {
+                regId: user.regId,
+                message: storage.getWord(user.userId)
+            };
+        });
+
+        //Q.all(messageModels);
+
+        sender.send(message, messageModels, function (err, result) {
+            if (err) console.error(err);
+            else console.log(result);
+        });
     });
 
-    // todo: get reg ids
-    var regIds = [''];
-    sender.send(message, regIds, function (err, result) {
-        if (err) console.error(err);
-        else console.log(result);
-    });
+    function _loadUsers() {
+        var deferred = Q.defer();
+        db.collection('users').find({}, function (err, res) {
+            if (err) {
+                console.log("Error during getting users: ", util.format(err.stack));
+                return deferred.reject(err);
+            }
+            console.log("Users were successfully loaded.");
+            deferred.resolve(res);
+        });
+    }
 }
